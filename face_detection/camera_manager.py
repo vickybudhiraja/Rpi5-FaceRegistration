07ea9cb68124acbfd712b371e781b2dcc68da12e
Vicky Budhiraja <vicky.vigia@gmail.com>
2025-03-04 06:28:12 +0000

    feat: gstreamer pipeline impl.

--- face_detection/camera_manager.py
+++ face_detection/camera_manager.py
@@ -5,16 +5,27 @@
 import cv2
 
 class CameraManager:
-    def __init__(self, camera_index=0, width=640, height=480):
-        self.camera_index = camera_index
+    def __init__(self, width=640, height=480, framerate=30):
         self.width = width
         self.height = height
+        self.framerate = framerate
         self.cap = None
 
+    ## my GStreamer pipeline for the Raspberry Pi Cam
+    def gstreamer_pipeline(self):
+        return (
+            f"libcamerasrc ! video/x-raw,format=NV12,width={self.width},height={self.height},framerate={self.framerate}/1 ! "
+            "videoconvert ! video/x-raw,format=BGR ! appsink"
+        )
+
+    ## open the gs-pipeline
     def open_camera(self):
-        self.cap = cv2.VideoCapture(self.camera_index)
-        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
-        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.cap = cv2.VideoCapture(self.gstreamer_pipeline(), cv2.CAP_GSTREAMER)
+
+        if not self.cap.isOpened():
+            print("err")
+            return False
+        return True
 
     def read_frame(self):
         if self.cap is not None and self.cap.isOpened():
@@ -26,3 +37,4 @@ class CameraManager:
     def close_camera(self):
         if self.cap is not None and self.cap.isOpened():
             self.cap.release()
+            print("system ended")
